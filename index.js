c86f5f08b14d31391a30c74f0ac7e74353fa095d
JowelislamHabib <jowel@bintofajjal.com>
2026-05-10 03:01:19 +0600

    Add MongoDB connection and update dependencies

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const PORT = process.env.PORT;
-
+const uri = process.env.MONGO_URI;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
