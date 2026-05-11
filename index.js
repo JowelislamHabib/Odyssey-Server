eff69adb57622a1fd3c3bb91dd2d0b2b933f0e5f
JowelislamHabib <jowel@bintofajjal.com>
2026-05-11 15:50:57 +0600

    Fix PATCH endpoint route and implement DELETE endpoint for destinations

--- index.js
+++ index.js
@@ -44,14 +44,24 @@ async function run() {
       res.json(result);
     });
 
-    app.patch("destination/:id", async (req, res) => {
+    app.patch("/destination/:id", async (req, res) => {
       const { id } = req.params;
       const updateData = req.body;
+      // console.log(updateData);
 
       const result = await destinationCollection.updateOne(
         { _id: new ObjectId(id) },
         { $set: updateData },
       );
+      res.json(result);
+    });
+
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await destinationCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
 
       res.json(result);
     });
