0a91c6b1bc25a52c6d84f6d98837e82d10f9791d
JowelislamHabib <jowel@bintofajjal.com>
2026-05-14 18:54:43 +0600

    Add authorization middleware to GET destination by ID endpoint

--- index.js
+++ index.js
@@ -35,15 +35,26 @@ async function run() {
       res.json(result);
     });
 
-    app.get(`/destination/:id`, async (req, res) => {
-      const { id } = req.params;
+    app.get(
+      `/destination/:id`,
+      (req, res, next) => {
+        const header = req.headers.authorization;
+        if (header === "yoyo") {
+          next();
+        } else {
+          res.status(401).json({ message: "Unauthorized" });
+        }
+      },
+      async (req, res) => {
+        const { id } = req.params;
 
-      const result = await destinationCollection.findOne({
-        _id: new ObjectId(id),
-      });
+        const result = await destinationCollection.findOne({
+          _id: new ObjectId(id),
+        });
 
-      res.json(result);
-    });
+        res.json(result);
+      },
+    );
 
     app.patch("/destination/:id", async (req, res) => {
       const { id } = req.params;
