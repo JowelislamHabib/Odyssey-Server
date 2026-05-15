a6d1dcefbff91db33b27ca48a978eb61aa88b9eb
JowelislamHabib <jowel@bintofajjal.com>
2026-05-16 05:07:32 +0600

    Add /featured endpoint to retrieve three featured destinations

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ async function run() {
     const destinationCollection = db.collection("destinations");
     const bookingCollection = db.collection("bookings");
 
+    app.get("/featured", async (req, res) => {
+      const result = await destinationCollection.find().limit(3).toArray();
+      res.json(result);
+    });
+
     app.get("/destination", async (req, res) => {
       const result = await destinationCollection.find().toArray();
       res.json(result);
